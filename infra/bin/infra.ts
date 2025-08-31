88b7ab7d4cb23000a8eb40ee6d9ea603ffec7ead
Emmanuel Isenah <emmanuelisenah@gmail.com>
2025-08-31 22:57:10 +0100

    fix: change '.js' import to '.ts'

--- infra/bin/infra.ts
+++ infra/bin/infra.ts
@@ -1,15 +1,11 @@
 #!/usr/bin/env node
 import * as cdk from "aws-cdk-lib";
-import { InfraStack } from "../lib/infra-stack";
+import { InfraStack } from "../lib/infra-stack.ts";
 
 const app = new cdk.App();
 new InfraStack(app, "SkewProtectionStack", {
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
     region: process.env.CDK_DEFAULT_REGION,
   },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -1,7 +1,7 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
-import { BackendStack } from "./backend-stack.js";
-import { FrontendStack } from "./frontend-stack.js";
+import { BackendStack } from "./backend-stack.ts";
+import { FrontendStack } from "./frontend-stack.ts";
 
 export class InfraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
